0e0b91333c4367b3cf8875ec764a802e0e44657e
Chris Breiding <chrisbreiding@gmail.com>
2012-03-29 20:36:46 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Newtv::Application.config.secret_token = '7c97c728009ed82e432067905e9ae2f75328537143f3054145aa43d12e585ce9ec6f7eae4ef723c87fba0885283868dfa12c69dbd5de3f0929b3aa17a4d9b564'
