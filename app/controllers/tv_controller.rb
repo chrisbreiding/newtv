8ced8620cb4e2e44e2e7fbf6e0c171db6b944606
Chris Breiding <chrisbreiding@gmail.com>
2012-03-30 14:16:31 -0400

    attempt #2

--- app/controllers/tv_controller.rb
+++ app/controllers/tv_controller.rb
@@ -1,12 +1,45 @@
 class TvController < ActionController::Base
+	
+	def initialize
+	end
+	
 	def index
 		require 'open-uri'
-	
-		xml = Nokogiri::XML(open('http://services.tvrage.com/feeds/episode_list.php?sid=28304'))
 		
-		@episodes = xml.xpath('//episode/title').map do |title|
-			title.text
+		ids = { 
+			'New Girl' => '28304',
+			'Modern Family' => '22622',
+			'The Office' => '6061'
+		}
+		
+# 		@episodes = []
+# 		
+# 		url = "http://services.tvrage.com/feeds/episode_list.php?sid=28304"
+# 		
+# 		xml = Nokogiri::XML(open(url))
+# 		
+# 		xml.xpath('//episode/title').each do |title|
+# 			@episodes.push title.text
+# 		end		
+		
+		@shows = []
+		
+		ids.each do |name, id|
+		
+			episodes = []
+					
+			url = "http://services.tvrage.com/feeds/episode_list.php?sid=#{id}"
+			
+			xml = Nokogiri::XML(open(url))
+			
+			xml.xpath('//episode/title').each do |title|
+				episodes.push title.text
+			end
+
+			@shows.push( { name: name, episodes: episodes } )
+			
 		end
+				
 	end
 
 end
