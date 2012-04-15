1329052dfe9b8518489f51319ddc8485f3a716aa
Chris Breiding <chrisbreiding@gmail.com>
2012-04-15 11:33:55 -0400

    Change cron:sync back to deleting episodes instead of dropping and re-creating table

--- lib/tasks/sync.rake
+++ lib/tasks/sync.rake
@@ -1,21 +1,7 @@
 namespace :cron do
 	
 	task :sync => :environment do
-		require 'tvrage'
-		
-		c = ActiveRecord::Base.connection
-		c.drop_table :episodes
-		c.create_table :episodes do |t|
-			t.integer :season
-			t.string :episode_number
-			t.string :title
-			t.integer :show_id
-			t.date :airdate
-			
-			t.timestamps
-		end
-		c.add_index :episodes, :show_id
-
+		require 'tvrage'		
 		TvRage.sync
 	end
 
--- lib/tvrage.rb
+++ lib/tvrage.rb
@@ -3,33 +3,27 @@ module TvRage
 	def self.sync(new_show = nil)
 		require 'open-uri'
 
-#		Episode.delete_all if new_show.nil?
+		Episode.delete_all if new_show.nil?
 		
 		shows = new_show ? [new_show] : Show.all
 
  		shows.each do |show|
- 			url = "http://services.tvrage.com/feeds/episode_list.php?sid=#{show[:tvrage_id]}"
-			xml = Nokogiri::XML(open(url))
+			xml = Nokogiri::XML(open("http://services.tvrage.com/feeds/episode_list.php?sid=#{show[:tvrage_id]}"))
 			
 			# update show with latest season
-			latest_season = xml.xpath('//totalseasons').text
-			show.update_attributes seasons: latest_season
+			show.update_attributes seasons: xml.xpath('//totalseasons').text
 			
 			# add all the episodes
 			xml.xpath('//Season').each do |season|
-				season_no = season.get_attribute('no').to_i
-				
 				season.children.each do |episode|
-					
 					details = {}
-					
 					episode.children.each do |detail|
 						details[detail.name] = detail.text
 					end
 					
 					Episode.create(
 						show_id:		show[:id],
-						season: 		season_no,
+						season: 		season.get_attribute('no').to_i,
 						episode_number: details['seasonnum'],
 						title: 			details['title'],
 						airdate: 		details['airdate']
