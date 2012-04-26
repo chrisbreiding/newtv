96aaf30d75910baeb2b4eb3cbd1d751e2fefd1d7
Chris Breiding <chrisbreiding@gmail.com>
2012-04-26 00:16:19 -0400

    Debugging undefined method airdate error

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,10 +1,10 @@
 class ShowsController < ActionController::Base
-	
+
 	layout 'application'
-	
+
 	def index
 		@shows = Show.includes(:episodes).where('episodes.airdate > ?', 3.days.ago ).order('airdate ASC')
-		
+
 		@shows.sort! do |a,b|
 			a.next_episode <=> b.next_episode
 		end
@@ -13,23 +13,23 @@ class ShowsController < ActionController::Base
 			@shows.select { |s| s.name == show.name }.empty?
 		end
 	end
-	
+
 	def show
 		@episodes = Show.find(params[:id]).episodes
-		
+
 		respond_to do |format|
 			format.html
 			format.json { render json: @episodes }
 		end
 	end
-	
+
 	def create
 		require 'tvrage'
-	
+
 		@show = Show.new(params[:show])
-		
+
 		TvRage.sync @show
-		
+
 		respond_to do |format|
 			if @show.save
 				format.html { redirect_to @show }
@@ -40,10 +40,10 @@ class ShowsController < ActionController::Base
 			end
 		end
 	end
-	
+
 	def update
 		@show = Show.find(params[:id])
-	
+
 		respond_to do |format|
 			if @show.update_attributes({ name: params[:name] })
 				format.html  { redirect_to @show }
@@ -54,10 +54,10 @@ class ShowsController < ActionController::Base
 			end
 		end
 	end
-	
+
 	def destroy
 		@show = Show.find(params[:id])
-		
+
 		respond_to do |format|
 			if @show.destroy
 				format.html  { redirect_to @show }
@@ -67,45 +67,45 @@ class ShowsController < ActionController::Base
 				format.json  { render json: @show.errors, status: :unprocessable_entity }
 			end
 		end
-	end	
+	end
 
 	def search
-		
+
 		require 'open-uri'
-		
+
 		show_name = params[:name]
-		
+
 		uri_safe = URI.escape show_name
 
 		url = "http://services.tvrage.com/feeds/search.php?show=#{uri_safe}"
-		
+
 		xml = Nokogiri::XML(open(url))
 
 		tags = %w{showid country name started seasons classification}
-				
+
 		@results = []
-		
+
 		xml.xpath('//show').each do |show|
-						
+
 			details = {}
-			
+
 			show.children.each do |detail|
-				
+
 				if tags.index detail.name
 					details[detail.name] = detail.text
 				end
-				
+
 			end
-			
+
 			@results << details
-			
+
 		end
-		
+
 		respond_to do |format|
 			format.html
 			format.json { render json: @results }
 		end
-				
+
 	end
-	
+
 end
