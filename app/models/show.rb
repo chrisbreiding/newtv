b9ee84528da7560289449decf0c481e82fdc8552
Chris Breiding <chrisbreiding@gmail.com>
2012-04-27 19:32:15 -0400

    Fixed another issue with next_episodes_airdate

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,14 +1,14 @@
 class Show < ActiveRecord::Base
 	validates_presence_of :name, :tvrage_id
-	has_many :episodes, dependent: :destroy		
-	
-	def next_episodes_airdate
-		i = 0
-		while self.episodes[i] && Time.parse(self.episodes[i].airdate.to_s) < 1.day.ago
-			# if there was a recently aired episode, but no scheduled future episodes
-			return 1.month.from_now if !self.episodes[i+1]
-			i += 1
-		end
-		self.episodes[i].airdate
-	end
+	has_many :episodes, dependent: :destroy
+
+  def next_episodes_airdate
+    i = 0
+    while self.episodes[i] && Time.parse(self.episodes[i].airdate.to_s) < 1.day.ago
+      # if there was a recently aired episode, but no scheduled future episodes
+      return Time.parse 1.month.from_now.to_s if !self.episodes[i + 1]
+      i += 1
+    end
+    Time.parse self.episodes[i].airdate.to_s
+  end
 end
