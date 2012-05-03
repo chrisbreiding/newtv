118cf79494e54283a6822532bc67d674a7d729b7
Chris Breiding <chrisbreiding@gmail.com>
2012-05-03 19:37:21 -0400

    Add status message to cron:sync

--- lib/tvrage.rb
+++ lib/tvrage.rb
@@ -3,16 +3,21 @@ module TvRage
 	def self.sync(new_show = nil)
 		require 'open-uri'
 
+    puts "deleting episodes..."
 		Episode.delete_all if new_show.nil?
-		
+
 		shows = new_show ? [new_show] : Show.all
 
  		shows.each do |show|
 			xml = Nokogiri::XML(open("http://services.tvrage.com/feeds/episode_list.php?sid=#{show[:tvrage_id]}"))
-			
+
+      puts "updating #{show.name}..."
+
+      puts "updating number of seasons"
 			# update show with latest season
 			show.update_attributes seasons: xml.xpath('//totalseasons').text
-			
+
+      puts "updating episodes"
 			# add all the episodes
 			xml.xpath('//Season').each do |season|
 				season.children.each do |episode|
@@ -20,7 +25,7 @@ module TvRage
 					episode.children.each do |detail|
 						details[detail.name] = detail.text
 					end
-					
+
 					Episode.create(
 						show_id:		show[:id],
 						season: 		season.get_attribute('no').to_i,
@@ -30,7 +35,7 @@ module TvRage
 					) unless details['title'].nil?
 				end
 			end
-		end	
-	end 
-	
+		end
+	end
+
 end
