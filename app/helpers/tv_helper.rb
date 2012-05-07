7fc673c30f0c2c055ce70aab40896f860e660ef1
Chris Breiding <chrisbreiding@gmail.com>
2012-05-07 17:08:32 -0400

    Add titlecase and filesafe helpers

--- app/helpers/tv_helper.rb
+++ app/helpers/tv_helper.rb
@@ -32,4 +32,22 @@ module TvHelper
 		end
 		num
 	end
+
+	def titlecase title
+    lowercase = %w{Of of Etc etc And and By by The the For for Is is At at To to But but Nor nor Or or A a An an Via via}
+    uppercase = %{TBA tba}
+    title.split(' ').each_with_index.collect { |word, i|
+    	if lowercase.include?(word) && i > 0
+    		word.downcase
+    	elsif uppercase.include?(word)
+    		word.upcase
+    	else
+    		word.capitalize
+    	end
+    }.join(' ')
+  end
+
+  def filesafe title
+    	title.gsub(/[\.\!\?\@\#\$\%\^\&\*]/, '')
+  end
 end
