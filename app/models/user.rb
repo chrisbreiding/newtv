26c67a832832e8eef1cea267a5cb9fe88ef84fba
Chris Breiding <chrisbreiding@gmail.com>
2012-06-02 21:10:46 -0400

    Add registerable to Devise to create production user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable, :recoverable, :trackable,
-  # :lockable, :timeoutable, :validatable, :registerable and :omniauthable
-  devise :database_authenticatable, :rememberable
+  # :lockable, :timeoutable, :validatable and :omniauthable
+  devise :database_authenticatable, :rememberable, :registerable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
