87eb772c22a543de6b5f63714cb25f6a1cc320d3
Chris Breiding <chrisbreiding@gmail.com>
2013-05-09 22:32:00 -0400

    fix flash notice

--- app/helpers/tv_helper.rb
+++ app/helpers/tv_helper.rb
@@ -1,22 +1,26 @@
 module TvHelper
 
   def flash_notice notice, type
-      raw(
-        content_tag(:div, id: 'notice', class: "notice clearfix #{type}") do
-          content_tag(:p, notice) +
-          link_to('Close notice', '#', id: 'close-notice', class: 'close-notice')
-        end
-      )
+    content = content_tag(:div, id: 'notice', class: "notice clearfix #{type}") do
+      notice_text = content_tag :p, content_tag(:span, nil, class: 'notice-dot') + notice
+      close_link = link_to('Close notice', '#', id: 'close-notice', class: 'close-notice')
+
+      notice_text + close_link
+    end
+
+    raw content
   end
 
 	def options show
-		raw(
-			content_tag(:span, class: 'options') do
-				link_to('Show All Episodes', show_path(show), class: 'show-all-episodes', title: 'Show All Episodes') +
-				link_to('Edit', edit_show_path(show), class: 'edit-show', title: 'Edit Show') +
-				link_to('Pirate Bay', "http://thepiratebay.se/search/#{show.name}", class: 'p-bay', target: '_blank', title: 'Pirate Bay Search')
-			end
-		)
+		content = content_tag(:span, class: 'options') do
+			all = link_to('Show All Episodes', show_path(show), class: 'show-all-episodes', title: 'Show All Episodes')
+			edit = link_to('Edit', edit_show_path(show), class: 'edit-show', title: 'Edit Show')
+			pb = link_to('Pirate Bay', "http://thepiratebay.se/search/#{show.name}", class: 'p-bay', target: '_blank', title: 'Pirate Bay Search')
+
+      all + edit + pb
+		end
+
+    raw content
 	end
 
   def episode_status_class airdate
