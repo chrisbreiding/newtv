1b2e20a56a6e6b1fbcf4c69ffb7e631332682526
Chris Breiding <chrisbreiding@gmail.com>
2013-05-13 22:52:10 -0400

    fix flash messages, move to partial

--- app/helpers/tv_helper.rb
+++ app/helpers/tv_helper.rb
@@ -1,16 +1,5 @@
 module TvHelper
 
-  def flash_notice notice, type
-    content = content_tag(:div, id: 'notice', class: "notice clearfix #{type}") do
-      notice_text = content_tag :p, content_tag(:span, nil, class: 'notice-dot') + notice
-      close_link = link_to('Close notice', '#', id: 'close-notice', class: 'close-notice')
-
-      notice_text + close_link
-    end
-
-    raw content
-  end
-
   def episode_status_class airdate
     if airdate == Date.today
       ' airs-today'
