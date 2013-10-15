566683fbe8befc338dc47ff03abbda239e2f0d06
Chris Breiding <chrisbreiding@gmail.com>
2013-10-15 19:38:23 -0400

    refactor tvsource

--- lib/tvsource.rb
+++ lib/tvsource.rb
@@ -1,4 +1,5 @@
 module TvSource
+  require 'open-uri'
 
   BASE_URL = "http://thetvdb.com/api/"
   API_KEY = "41066016732D1E20"
@@ -15,31 +16,11 @@ module TvSource
   end
 
   def self.update_episodes_for_show(show)
-    require 'open-uri'
     require 'timeout'
 
-    retries = 0
-    url = URLS[:show_zip].sub(/:show_id/, show[:tvsource_id])
-
     Rails.logger.debug show.name
-    begin
-      Timeout::timeout(20) do
-        Rails.logger.debug "... grabbing xml - attempt ##{retries + 1}"
-        remote_data = open(url).read
-        local_file = open("#{Rails.root}/en.zip", "wb")
-        local_file.write(remote_data)
-        local_file.close
-      end
-    rescue Timeout::Error
-      retries += 1
-      if retries <= 15
-        sleep 5 and retry
-      else
-        Rails.logger.debug "!.. failed after 15 attempts"
-        raise
-      end
-    end
 
+    download_url( URLS[:show_zip].sub(/:show_id/, show[:tvsource_id]) )
     xml = Nokogiri::XML(show_xml_from_zip)
 
     if !xml.nil?
@@ -50,61 +31,84 @@ module TvSource
 
       Rails.logger.debug "... creating episodes"
       xml.css('Episode').each do |episode|
-        season         = episode.css('SeasonNumber')
-        episode_number = episode.css('EpisodeNumber')
-        title          = episode.css('EpisodeName')
-        airdate        = episode.css('FirstAired')
-
-        Episode.create(
-          show_id:        show[:id],
-          season:         season.empty? ? 9999 : season[0].text,
-          episode_number: episode_number.empty? ? 9999 : episode_number[0].text,
-          title:          title.empty? ? '' : title[0].text,
-          airdate:        airdate.empty? ? '1970-01-01' : airdate[0].text
-        )
+        details = details_for_episode(episode)
+        details[:show_id] = show[:id]
+        Episode.create(details)
       end
     else
       Rails.logger.debug "!.. xml was nil"
     end
   end
 
-  def self.show_xml_from_zip
-    require 'zip'
-    Zip::File.open("#{Rails.root}/en.zip").read('en.xml')
-  end
-
   def self.search(show_name)
-    require 'open-uri'
-
     url = URLS[:show_search].sub(/:show_name/, show_name)
     xml = Nokogiri::XML(open(URI.escape(url)))
 
-    attributes = %w{seriesid SeriesName Overview FirstAired Network}
-    attributes = {
-      "seriesid" => "id",
-      "SeriesName" => "name",
-      "Overview" => "overview",
-      "FirstAired" => "started",
-      "Network" => "network"
-    }
-
-    xml.xpath('//Series').collect do |show|
-      details = {}
-
-      show.children.each do |detail|
-        if attributes.key?(detail.name)
-          details[attributes[detail.name]] = detail.text
-        end
-      end
-
-      attributes.each do |key, value|
-        if !details.key?(value)
-          details[value] = "unknown"
-        end
-      end
-
-      details
+   xml.xpath('//Series').collect do |show|
+      details_for_show(show)
     end
   end
+
+  private
+
+    def self.download_url(url)
+      retries = 0
+      begin
+        Timeout::timeout(20) do
+          Rails.logger.debug "... grabbing xml - attempt ##{retries + 1}"
+          remote_data = open(url).read
+          local_file = open("#{Rails.root}/en.zip", "wb")
+          local_file.write(remote_data)
+          local_file.close
+        end
+      rescue Timeout::Error
+        retries += 1
+        if retries <= 15
+          sleep 5 and retry
+        else
+          Rails.logger.debug "!.. failed after 15 attempts"
+          raise
+        end
+      end
+    end
+
+    def self.show_xml_from_zip
+      require 'zip'
+      Zip::File.open("#{Rails.root}/en.zip").read('en.xml')
+    end
+
+    def self.details_for_show(show)
+      attributes = [
+        { selector: 'seriesid',   property: 'id',       default: ''        },
+        { selector: 'SeriesName', property: 'name',     default: 'Unknown' },
+        { selector: 'Overview',   property: 'overview', default: ''        },
+        { selector: 'FirstAired', property: 'started',  default: ''        },
+        { selector: 'Network',    property: 'network',  default: ''        }
+      ]
+      details_for_node(show, attributes)
+    end
+
+    def self.details_for_episode(episode)
+      attributes = [
+        { selector: 'SeasonNumber',  property: 'season',         default: 0            },
+        { selector: 'EpisodeNumber', property: 'episode_number', default: '00'         },
+        { selector: 'EpisodeName',   property: 'title',          default: ''           },
+        { selector: 'FirstAired',    property: 'airdate',        default: '1970-01-01' }
+      ]
+      details_for_node(episode, attributes)
+    end
+
+    def self.details_for_node(node, attributes)
+        details = {}
+        attributes.each do |attribute|
+          detail = node.css(attribute[:selector])
+          details[attribute[:property]] = if detail.empty?
+            attribute[:default]
+          else
+            detail[0].text
+          end
+        end
+        details
+    end
 
 end
